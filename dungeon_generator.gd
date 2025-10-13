7faec8e492177852eca69590ac1e34ec9a2c2c6e
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-12 19:37:02 -0600

    basic expansion of room exits to others

--- dungeon_generator.gd
+++ dungeon_generator.gd
@@ -1,22 +1,56 @@
 extends Node3D
-@export var rooms: Array[PackedScene] = [] # 1 is the starting scene 
+@export var starting_room: PackedScene
+@export var random_rooms: Array[PackedScene] = [] # 1 is the starting scene 
 
 func get_random_room() -> Node3D:
-	return rooms[randi() % rooms.size()].instantiate()
+	return random_rooms[randi() % random_rooms.size()].instantiate()
 
 func expand():
-	var exits := get_tree().get_nodes_in_group("exit")
-	#print("Found exits:", exits)
+	var exits := get_tree().get_nodes_in_group("room_exit")
 	
 	for exit in exits:
 		var new_room = get_random_room()
 		add_child(new_room)
 
-		new_room.global_transform.origin = exit.global_transform.origin
+		new_room.global_transform = exit.global_transform
 		new_room.rotation_degrees.y += 180
+		
+		var valid = not new_room.intersecting_area("room_intersecting"); #print(valid)
+		
+
+		if valid:
+			
+			exit.remove_from_group("room_exit")
+			
+			for child in new_room.get_children():
+				if child.is_in_group("room_exit"):
+					child.remove_from_group("room_exit")
+					break  
+			
+		else:
+			new_room.queue_free()
+			
+			
+func genrate_dungeon():
+	for child in get_children(): child.queue_free()
+	await get_tree().process_frame
+	await get_tree().physics_frame
+	
+	var new_room = get_random_room()
+	add_child(new_room)
+	
+	
+	
+	for i in range(3):
+		await get_tree().create_timer(1.0).timeout
+		await expand()
+
+	#var all_exits = get_tree().get_nodes_in_group("room_exit")
+	#print("Total exits after generation:", all_exits.size())
 
 func _ready():
-
-	add_child(get_random_room())
+	genrate_dungeon()
 	
-	expand()	
+func _process(_delta):
+	if OS.is_debug_build() and Input.is_action_just_pressed("restart"):
+		genrate_dungeon()
--- rooms/room.gd
+++ rooms/room.gd
@@ -1,3 +1,16 @@
 extends Node3D
 @export var area: Area3D ## Used to tell if this room is intersecting with another 
 @export var grid_map: GridMap
+
+func intersecting_area(area_group_name: String) -> bool:
+	var params = PhysicsShapeQueryParameters3D.new()
+	params.shape = area.shape_owner_get_shape(0, 0)
+	params.transform = area.global_transform
+	params.collide_with_areas = true
+	params.collide_with_bodies = false
+	var space = get_world_3d().direct_space_state
+	
+	for r in space.intersect_shape(params, 32): 
+		if r.collider.is_in_group(area_group_name):
+			return true
+	return false
