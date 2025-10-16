8157e4bba0e71cc7d9dfd39eee49af2a3ae7ef8c
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-16 04:50:51 -0600

    complete random outfits for everyone, just needing more variation and masks

--- guests/guest.gd
+++ guests/guest.gd
@@ -2,25 +2,26 @@ extends Node3D
 @export var mesh: MeshInstance3D
 @export var skeleton: Skeleton3D
 
+#@exportsubgroup
+@export var skin_materials: Array[StandardMaterial3D] = []
+@export var dress_materials: Array[StandardMaterial3D] = []
+@export var hair_materials: Array[StandardMaterial3D] = []
+
 @export var random_gender: bool = true
 @export var male_blend: float = 0.0 ## 0 is Female 1 is Male
 
 @export var random_hair: bool = true
 @export var hair_index: int = -1
-
-
-@export var skin_materials: Array[StandardMaterial3D] = []
-
-
-@export var dress_materials: Array[StandardMaterial3D] = []
-@export var female_dresses_gltf: Array[PackedScene] = []
-
-@export var hair_materials: Array[StandardMaterial3D] = []
 @export var female_hair_gltf: Array[PackedScene] = []
 @export var male_hair_gltf: Array[PackedScene] = []
+
+@export var female_outfits_gltf: Array[PackedScene] = []
+@export var male_outfits_gltf: Array[PackedScene] = []
 var hair
 
 func add_skinned_mesh_gltf_to_skeleton(gltf_scene: PackedScene, skeleton: Skeleton3D) -> MeshInstance3D:
+	if gltf_scene == null: return null
+	
 	var instance = gltf_scene.instantiate()
 	add_child(instance)
 	var mesh = instance.get_child(0).get_child(0).get_child(0) as MeshInstance3D
@@ -35,12 +36,15 @@ func add_skinned_mesh_gltf_to_skeleton(gltf_scene: PackedScene, skeleton: Skelet
 	instance.queue_free()
 	return mesh
 
-func set_mesh_material(mesh: MeshInstance3D, materials: Array[StandardMaterial3D]) -> void:
-	if materials.size() == 0:
-		return
-	for i in mesh.mesh.get_surface_count():
-		var mat = materials[randi() % materials.size()].duplicate()
-		mesh.mesh.surface_set_material(i, mat)
+func set_mesh_material(mesh: MeshInstance3D, materials: Array[StandardMaterial3D], slot: int = -1) -> void:
+	if mesh == null or mesh.mesh == null: return
+	if materials.size() == 0: return
+	var mat = materials[randi() % materials.size()].duplicate()
+	if slot >= 0 and slot < mesh.mesh.get_surface_count():
+		mesh.mesh.surface_set_material(slot, mat)
+	else:
+		for i in range(mesh.mesh.get_surface_count()):
+			mesh.mesh.surface_set_material(i, mat)
 
 func _ready():
 	var value := male_blend
@@ -49,20 +53,18 @@ func _ready():
 	mesh.set("blend_shapes/Male", value)
 	male_blend = mesh.get("blend_shapes/Male")
 	
-	set_mesh_material(mesh, skin_materials)
+	set_mesh_material(mesh, skin_materials, 0)
 	
 	if male_blend == 0.0:
 		hair = add_skinned_mesh_gltf_to_skeleton(female_hair_gltf.pick_random(), skeleton)
 		
-		var dress_mesh = add_skinned_mesh_gltf_to_skeleton(female_dresses_gltf.pick_random(), skeleton)
-		set_mesh_material(dress_mesh, dress_materials)
+		var female_outfit_mesh = add_skinned_mesh_gltf_to_skeleton(female_outfits_gltf.pick_random(), skeleton)
+		set_mesh_material(female_outfit_mesh, dress_materials)
+	else:
+		hair = add_skinned_mesh_gltf_to_skeleton(male_hair_gltf.pick_random(), skeleton)
+		
+		add_skinned_mesh_gltf_to_skeleton(male_outfits_gltf.pick_random(), skeleton)
 
 	if hair: set_mesh_material(hair, hair_materials)
 	
 	
-	#hair.queue_free()
-
-	
-	#if random_hair:
-		#if male_blend == 0.0: hair_index = randi() % female_hair.size()
-		#else: hair_index = randi() % male_hair.size()
