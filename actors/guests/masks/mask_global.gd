6ed5f549e937aae6bc5fd5ec169485b48755efeb
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-18 14:53:04 -0600

    mask variation, faster mask importing and itteration. Mask also use different color. Ui also clearer on goal

--- actors/guests/masks/mask_global.gd
+++ actors/guests/masks/mask_global.gd
@@ -1,9 +1,13 @@
 extends Node
 @export var target_index: int = -1
+@export var random_materials: Array[int] = [1,2,3,4]
+
 @export var masks_needed_for_completion: int = 20
 
 func set_random_target():
 	target_index = randi()
+	for i in range(4):
+		random_materials.append(randi())
 
 func _ready():
 	Save.data["venge"] = 0
--- actors/guests/show_correct_mask.gd
+++ actors/guests/show_correct_mask.gd
@@ -1,24 +1,50 @@
 extends Node
 @export var masks: Node3D
 @export var wearing_target_mask: bool = false
+@export var mask_parent: Node3D
+@export var mask_scenes: Array[PackedScene] = []
+@export var look_at_modifier: LookAtModifier3D
+@export var mask_materials: Array[StandardMaterial3D] = []
+var mask
 
 func _ready():
 
-	var mask_list = masks.get_children()
-	for m in mask_list: m.visible = false
-	
-	var normalized_index = Mask.target_index % mask_list.size() # Normalize inside size of array
-	var random_index = randi() % mask_list.size()
-	if random_index == normalized_index:
-		random_index = (random_index + 1) % mask_list.size()
-	
-	if wearing_target_mask: mask_list[normalized_index].visible = true
-	else: mask_list[random_index].visible = true
-	
-	for i in range(mask_list.size()):
-		var mask = mask_list[i]
-		if not mask.visible: mask.queue_free()
-	
+	var old_influence = 1.0 # bandaid fix look at modifier messing up initial placement 
+	if look_at_modifier:
+		look_at_modifier.influence 
 
+	if not mask_scenes.size() > 0: return
+
+	var normalized_index = Mask.target_index % mask_scenes.size() # Normalize inside size of array
+	var random_index = randi() % mask_scenes.size()
+	if random_index == normalized_index:
+		random_index = (random_index + 1) % mask_scenes.size()
+		
+	var final_index
+	if wearing_target_mask: final_index = normalized_index
+	else: final_index = random_index	
+
+	var mask_gltf = mask_scenes[final_index].instantiate() # Extract mask from gltf scene
+	mask = mask_gltf.get_child(0)
+	mask_gltf.remove_child(mask)
+	mask_gltf.queue_free()
+	mask.owner = null
+	add_child(mask)
+	mask.global_transform = mask_parent.global_transform
+
+	if mask_materials.size() > 0:
+		if wearing_target_mask:
+			var surface_count = mask.mesh.get_surface_count()
+			for i in range(surface_count):
+				var mat_index = Mask.random_materials[i] % mask_materials.size()
+				mask.set_surface_override_material(i, mask_materials[mat_index])
+			
+		else:
+			var surface_count = mask.mesh.get_surface_count()
+			for i in range(surface_count):
+				mask.set_surface_override_material(i, mask_materials[randi() % mask_materials.size()])
+	
+	if look_at_modifier:
+		look_at_modifier.influence = old_influence
 
 	
